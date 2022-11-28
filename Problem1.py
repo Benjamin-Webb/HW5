fb9d541257cbde87b64aba245a5c08f74bea3f7b
Benjamin-Webb <benjamin.webb@asu.edu>
2022-11-28 15:24:08 -0700

    Began implementation of QP function

--- Problem1.py
+++ Problem1.py
@@ -4,33 +4,31 @@
 
 import numpy as np
 
-def objfun(x, k):
+def objfun(x):
 	# objective function
 	# x: 2x1 vector
-	# k: iteration number, int
 
-	return x[0, k]**2 + (x[1, k] - 3.0)**2
+	return x[0]**2 + (x[1] - 3.0)**2
 
-def constraints(x, k):
+def constraints(x):
 	# vector of inequality constraints
 	# x: 2x1 vector
-	# k: iteration number, int
 
 	g = np.zeros((2, 1), dtype=np.single)
-	g[0] = x[1, k]**2 - 2*x[0, k]
-	g[1] = (x[1, k] - 1.0)**2 + 5.0*x[0, k] - 15.0
+	g[0] = x[1]**2 - 2*x[0]
+	g[1] = (x[1] - 1.0)**2 + 5.0*x[0] - 15.0
 
 	return g
 
-def gradLagrangian(x, mu, k):
+def gradLagrangian(x, mu):
 	# Calculates gradient of the Lagrangian
 	# x: 2x1 vector
 	# g: 2x1 vector
 	# k: interation number, int
 
 	gradL = np.zeros((2, 1), dtype=np.single)
-	gradL[0] = 2*x[0, k] - 2*mu[0, k] + 5*mu[1, k]
-	gradL[1] = 2*x[1, k] - 6.0 + 2*mu[0, k]*x[1, k] + 2*mu[1, k]*x[1, k] - 2*mu[1, k]
+	gradL[0] = 2*x[0] - 2*mu[0] + 5*mu[1]
+	gradL[1] = 2*x[1] - 6.0 + 2*mu[0]*x[1] + 2*mu[1]*x[1] - 2*mu[1]
 
 	return gradL
 
@@ -51,7 +49,7 @@ def meritfun(x, mu, w, k):
 
 	return f + np.sum(w[:, k]*np.max(0.0, g))
 
-def QP(x, mu, W, k):
+def QP(x, mu, W):
 	# Solves QP subproblem w/ active set
 	# x: 2x1 vector
 	# mu: 2x1 vector
@@ -60,10 +58,16 @@ def QP(x, mu, W, k):
 	# Formulate intial set of active constraints
 	A = np.array([[-2.0, 2.0*x[1]],
 	              [5.0, 2.0*x[1] - 2.0]], dtype=np.single)
-	A = A*np.abs(np.sign(mu))   # If inactive, np.sign = 0... Giving A = 0 for constraint
+	if np.sign(mu[0]) < 0.0:
+		A[0] = 0.0
+	if np.sign(mu[1]) < 0.0:
+		A[1] = 0.0
 
-	gbar = constraints(x, k)
-	gbar = gbar*np.abs(np.sign(mu))    # If inactive, gbar = 0
+	gbar = constraints(x)
+	if np.sign(mu[0]) < 0.0:
+		gbar[0] = 0.0
+	if np.sign(mu[1]) < 0.0:
+		gbar[1] = 0.0
 
 	# Jacobian of objective function
 	fx = np.array([2*x[0], 2*x[1] - 6.0], dtype=np.single)
@@ -81,16 +85,25 @@ def QP(x, mu, W, k):
 		# Update sk and mu
 		sk = E[:2]
 		mu = E[2:4]
+		gbar = constraints(x)
 
 		# Update active constraints
 		if np.min(mu) <= 0.0:
 			idx = np.argmin(mu)
-			sign = np.ones(2, 1)
-			sign[idx] = 0.0
-			A = np.array([[-2.0, 2.0 * x[1]],
-			              [5.0, 2.0 * x[1] - 2.0]], dtype=np.single)*sign
+			if idx == 0:
+				A[0] = 0.0
+				gbar[0] = 0.0
+			elif idx == 1:
+				A[1] = 0.0
+				gbar[1] = 0.0
 		elif np.amax(A*sk + gbar) > 0.0:
+			idx = np.argmax(A*sk + gbar)
+			if idx == 0:
+				A[0] = np.array([-2.0, 2.0*x[1]])
+			elif idx == 1:
+				A[1] = np.array([5.0, 2.0*x[1] - 2.0])
 
+	return sk, mu
 
 if __name__ == "__main__":
 	# main script
@@ -98,19 +111,23 @@ if __name__ == "__main__":
 	# Iteration counter
 	k = np.uint16(0)
 
-	# Initial solution guess
+	# Initial solution guess and step size
+	sk = np.zeros((2, 1000), dtype=np.single)
 	x = np.zeros((2, 1000), dtype=np.single)
 	x[:, :1] = np.array([[1.0], [1.0]])
 
 	# Determine if any of the inequality constraints are active
 	g = np.zeros((2, 1000), np.single)
-	g[:, :1] = constraints(x, k)
+	g[:, :1] = constraints(x[:, k])
 	mu = np.zeros((2, 1000), np.single)
 
 	# Calculate gradient of Lagrangian at x0
 	gradL = np.zeros((2, 1000), dtype=np.single)
-	gradL = gradLagrangian(x, mu, k)
+	gradL[:, :1] = gradLagrangian(x[:, k], mu[:, k])
 
 	# Initialize W
 	W = np.zeros((2, 2, 1000), dtype=np.single)
 	W[:, :, :1] = np.eye(N=2, dtype=np.single)
+
+	# Test QP
+	[sk[:, k], mu[:, k]] = QP(x[:, k], mu[:, k], W[:, :, k])
