725268b6db6f084c26ae728239552b67936fc0a4
Benjamin-Webb <benjamin.webb@asu.edu>
2022-12-02 18:16:04 -0700

    Need to check

--- Problem1.py
+++ Problem1.py
@@ -105,7 +105,7 @@ def QP(x, mu, W, k):
 		# Determine if QP subproblem is solved
 		if mu[0] > 0:
 			if dgdx1 <= 0.0:
-				if mu[1] <= 0.0 and dgdx2 <= 0.0:
+				if mu[1] <= 0.0:
 					if mu[1] < 0.0:
 						mu[1] = 0.0
 					break
@@ -113,7 +113,7 @@ def QP(x, mu, W, k):
 					break
 		if mu[1] > 0:
 			if dgdx2 <= 0.0:
-				if mu[0] <= 0 or dgdx1 <= 0.0:
+				if mu[0] <= 0:
 					if mu[0] < 0.0:
 						mu[0] = 0.0
 					break
@@ -127,15 +127,15 @@ def QP(x, mu, W, k):
 			A[0, 0] = 0.0
 			A[0, 1] = 0.0
 			gbar[0] = 0.0
+		elif dgdx1 > 0.0 and dgdx1 > dgdx2:
+			A[0, 0] = -2.0
+			A[0, 1] = 2*x[1]
+			gbar[0] = x[1]**2 - 2*x[0]
 		if mu[1] <= 0.0 and mu[1] < mu[0]:
 			A[1, 0] = 0.0
 			A[1, 1] = 0.0
 			gbar[1] = 0.0
-		if dgdx1 > 0.0 and dgdx1 > dgdx2:
-			A[0, 0] = -2.0
-			A[0, 1] = 2*x[1]
-			gbar[0] = x[1]**2 - 2*x[0]
-		if dgdx2 > 0.0 and dgdx2 > dgdx1:
+		elif dgdx2 > 0.0 and dgdx2 > dgdx1:
 			A[1, 0] = 5.0
 			A[1, 1] = 2*x[1] - 2.0
 			gbar[1] = (x[1] - 1.0)**2 + 5*x[0] - 15.0
@@ -271,4 +271,4 @@ if __name__ == "__main__":
 		gradL[:, k+1:k+2] = gradLagrangian(x[:, k+1:k+2], mu[:, k+1:k+2])
 		k += 1
 
-		res = np.linalg.norm(gradL[:, k] - gradL[:, k-1])
+		res = np.linalg.norm((gradL[:, k] - gradL[:, k-1]/gradL[:, k-1]))
